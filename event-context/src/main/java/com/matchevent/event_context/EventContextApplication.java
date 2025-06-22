85d05dccb5657b15d5358548f0a8a474094ee6d4
ivanlamadrid <ivanlamadrid@outlook.com>
2025-06-21 22:29:26 -0500

    feat(event): initial skeleton for Event Context

---
+++ event-context/src/main/java/com/matchevent/event_context/EventContextApplication.java
@@ -0,0 +1,13 @@
+package com.matchevent.event_context;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventContextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventContextApplication.class, args);
+	}
+
+}
---
+++ event-context/src/test/java/com/matchevent/event_context/EventContextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matchevent.event_context;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventContextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
