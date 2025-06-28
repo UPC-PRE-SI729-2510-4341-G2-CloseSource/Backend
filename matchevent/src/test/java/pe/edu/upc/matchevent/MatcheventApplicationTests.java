1b4bc0289b43f2d91ac0768a3749a5073e5424c9
MaxPP <u201314454@upc.edu.pe>
2025-06-27 19:44:04 -0500

    feat (main): Application created

---
+++ matchevent/src/test/java/pe/edu/upc/matchevent/MatcheventApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.matchevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatcheventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
